13797491d5a5d67e4078e7a630d709ccae9bc5b5
stephband <stephband@googlemail.com>
2013-03-03 23:42:38 +0000

    Styles radio_buttons

--- js/noiseapp.views.source.js
+++ js/noiseapp.views.source.js
@@ -1,6 +1,7 @@
 (function(jQuery, app, Model, undefined){
 	app.views.source = function (node, model) {
 		var elem = jQuery(node),
+		    distElem = elem.find('.lp_dist'),
 		    output = elem.find('.source_output');
 		
 		model.on('output', function(model) {
@@ -8,7 +9,16 @@
 			output.html(Math.round(value) + ' dB');
 		});
 		
-		elem.data('model', model);
+		elem
+		.data('model', model)
+		.on('change', '[name="leveltype"]', function(e) {
+			if (e.currentTarget.value === "lp" && e.currentTarget.checked) {
+				distElem.removeClass('hidden');
+			}
+			else {
+				distElem.addClass('hidden');
+			}
+		});
 		
 		// Immediately update the view 
 		model.trigger('output');
