e3209c8540a44828e2252cd8d0490cf26a5a451e
goodhandd <daniel@goodhandacoustics.co.uk>
2013-03-03 00:56:32 +0000

    update sourcemodel and acousticfunctions.js

--- js/AcousticFunctions.js
+++ js/AcousticFunctions.js
@@ -1,164 +1,58 @@
-var ln = Math.log;
-var pow = Math.pow;
+(function(window){
 
-function log10(n) {
-  return ln(n) / ln(10);
-}
+	var ln = Math.log;
+	var pow = Math.pow;
 
-function dBAdd(n) {
+	function log10(n) {
+	  return ln(n) / ln(10);
+	}
 
-	var dBObj = dBCalc(arguments);
-	return 10*log10(dBObj.totpow);
+	function dBAdd(n) {
 
-}
+		var dBObj = dBCalc(arguments);
+		return 10*log10(dBObj.totpow);
 
-function dBAvg(n) {
+	}
 
-	var dBObj = dBCalc(arguments);
-	return 10*log10(dBObj.totpow/dBObj.totnum);
+	function dBAvg(n) {
 
-}
+		var dBObj = dBCalc(arguments);
+		return 10*log10(dBObj.totpow/dBObj.totnum);
 
-function dBCalc(n){
+	}
 
-	var ctrarg = n.length;
-	var arr;
-	var totpow = 0;
-	var totnum = 0;
-	
-	while(ctrarg--){
-	
-		arr = n[ctrarg];
+	function dBCalc(n){
+
+		var ctrarg = n.length;
+		var arr;
+		var totpow = 0;
+		var totnum = 0;
 		
-		if(arr instanceof Array){ 
+		while(ctrarg--){
+		
+			arr = n[ctrarg];
 			
-			var dBObj = dBCalc(arr);
-			totpow = dBObj.totpow + totpow;
-			totnum = dBObj.totnum + totnum;
+			if(arr instanceof Array){ 
+				
+				var dBObj = dBCalc(arr);
+				totpow = dBObj.totpow + totpow;
+				totnum = dBObj.totnum + totnum;
 
+			}
+			else{
+			
+				totpow = pow(10,arr/10)+totpow;
+				totnum++;
+			
+			}
 		}
-		else{
 		
-			totpow = pow(10,arr/10)+totpow;
-			totnum++;
-		
-		}
+		return {totpow:totpow, totnum:totnum};
 	}
 	
-	return {totpow:totpow, totnum:totnum};
-}
-
-// BS 5228-1 calculations
-
-var srcFacade;
-var recFacade;
-var srcNoiseLvl;
-var refDist;
-var dist;
-var barLineOfSite;
-var percOnTime;
-var recNoiseLvl;
-var swlBool;
-
-recFacade = True;
-srcFacade = False;
-swlBool = False;
-refDist = 10;
-
-function recNoiseLevel{
-
-	return srcNoiseLvl - attenuation;
-
-}
-
-
-function attenuation{
-	
-	var attvar = 0;
-	
-	if swlBool{
-	
-		attvar = khLw(dist);
-	
-	}
-	else{
-	
-		attvar = kh(refDist,dist);
-	
-	}
-	
-	 attvar = attvar + kpercOnTime(percOnTime) + kbar(barLineOfsite) + kfac(srcFacade,recFacade);
-	 
-	 return attvar;
-}
-
-function kfac(srcFacade,recFacade){
-
-	var kfacvar = 0;
-
-	if srcFacade = True{
-		kfacvar = kfacvar + 3;
-	}
-	
-	if recFacade = True{
-		kfacvar = kfacvar + 3;
-	}
-
-	return kfacvar;
-}
-
-function kbar(barLineOfSite){
-
-var kbarvar
-
-switch (barLineOfSite) {
-	case (barLineOfSite === "No barrier"):
-		kbarvar = 0;
-		break;
-	case (barLineOfSite === "Plant just visible"):
-		kbarvar = 5;
-		break;
-	case (barLineOfSite === "No line of site"):
-		kbarvar = 10;
-		break;
-	}	
-	return kbarvar;
-}
-function ksLw(r){
-
-	if (r>=25){
-		return 25*log10(r)+1;
-	}
-	else{
-		return khLw(r);
-	}
-
-}
-
-function khLw(r){
-
-	return 20*log10(r)+8;
-
-}
-
-function ks(d,r){
-
-	return 20*log(r/d);
-
-}
-
-function kh(d,r){
-
-	return 25*log(r/d)-2;
-
-}
-
-funtion kpercOnTime(p){
-
-	return 10*log10(1/p);
-
-}
-
-
-
-
+	window.acfns={
+		log10: log10
+		dBAdd: dBAdd
+		dBAvg: dBAvg
+	};
+})(window);
--- js/noiseapp.models.source.js
+++ js/noiseapp.models.source.js
@@ -1,10 +1,144 @@
 (function(jQuery, app, Model, undefined){
 	
+	var recFacade = true,
+	    srcFacade = false,
+	    swlBool = false,
+	    refDist = 10;
 	
 	
+	// Prototype Functions
+
+	
+
+	
+	function kbar(barLineOfSite){
+
+		var kbarvar
+
+		switch (barLineOfSite) {
+			case (barLineOfSite === 0):
+				kbarvar = 0;
+				break;
+			case (barLineOfSite === 1):
+				kbarvar = 5;
+				break;
+			case (barLineOfSite === 2):
+				kbarvar = 10;
+				break;
+		}		
+		return kbarvar;
+	}
+	
+	function kfac(srcFacade,recFacade){
+
+		var kfacvar = 0;
+
+		if (srcFacade === true){
+			kfacvar = kfacvar + 3;
+		}
+		
+		if (recFacade === true){
+			kfacvar = kfacvar + 3;
+		}
+
+		return kfacvar;
+	}
+	
+	function ksLw(r){
+
+		if (r>=25){
+			return 25*log10(r)+1;
+		}
+		else{
+			return khLw(r);
+		}
+
+	}
+
+	function khLw(r){
+
+		return 20*log10(r)+8;
+
+	}
+
+	function ks(d,r){
+
+		return 20*log(r/d);
+
+	}
+
+	function kh(d,r){
+
+		return 25*log(r/d)-2;
+
+	}
+
+	function kpercOnTime(p){
+
+		return 10*log10(1/p);
+
+	}
 	
 	function SourceModel(data, url) {
 		Model.apply(this, arguments);
+		
+		var kpercOnTimeVar = kpercOnTime(this.get('time')),
+			kbarVar = kbar(this.get('barrier'),
+			kfacVar = kfac(srcFacade,recFacade),
+			khVar,
+			srcNoiseLvl = this.get('level');
+		
+		distAttenuation(sourcemodel.get('distance',refDist))
+		recNoiseLevel(this);
+		
+		function recNoiseLevel(model){
+					
+			model.set('output', srcNoiseLvl - attenuation());
+		}	
+
+
+		
+		function attenuation(){
+		
+			return khVar + kpercOnTimeVar + kbarVar + kfacVar;
+		 
+		}
+		
+		
+		function distAttenuation(dist, refDist){
+	
+			if (swlBool){
+		
+				khVar = khLw(dist);
+		
+			}
+			else{
+		
+				khVar = kh(refDist,dist);
+		
+			};
+		}
+		
+		
+		this.on('level', function(sourcemodel){
+			srcNoiseLvl = sourcemodel.get('level');
+			recNoiseLevel(sourcemodel);
+		});
+		
+		this.on('distance', function(sourcemodel){
+			distAttenuation(sourcemodel.get('distance',refDist));
+			recNoiseLevel(sourcemodel);
+		});
+			
+		this.on('barrier', function(sourcemodel){
+			kbarvVar = kbar(this.get('barrier')));
+			recNoiseLevel(sourcemodel);
+		});
+		
+		this.on('time', function(sourcemode){
+			kpercVar = kpercOnTime(this.get('time'));
+			recNoiseLevel(sourcemodel);
+		});
 	};
 	
 	SourceModel.prototype = Object.create(Model.prototype);
